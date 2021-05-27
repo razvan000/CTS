4924cf85b1e4491f49d5892879ca9c52e7d5bbb1
Razvan <mrazvan99@yahoo.com>
2021-05-27 14:42:40 +0300

    Week 13 Unit Testing Conformance

---
+++ cts_seminar13 - Unit Testing/src/ro/ase/csie/cts/exceptions/WrongGradeExceptions.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.exceptions;
+
+public class WrongGradeExceptions extends Exception{
+
+}
---
+++ cts_seminar13 - Unit Testing/src/ro/ase/csie/cts/exceptions/WrongNameException.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.exceptions;
+
+public class WrongNameException extends Exception{
+
+}
